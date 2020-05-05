dd19449d4c563a858fe28ff488cc27138bc6d4b3
Piotr Omelianiuk <pomelianiuk@gmail.com>
2020-05-05 16:08:35 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+alert('siema')
+const name='Piotrek';
+const age= 25 ;
+
+console.log(`Siemanko. Miło, że tu zajrzałeś. Nazywam się ${name} i mam ${age} lat.`)
+console.log(name);
+console.log(age);
