1975ac76323f9f632aaf9210758c903713c1289e
Piotr Omelianiuk <pomelianiuk@gmail.com>
2020-05-12 23:41:35 +0200

    add JS HTML

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ const age= 25 ;
 console.log(`Siemanko. Miło, że tu zajrzałeś. Nazywam się ${name} i mam ${age} lat.`);
 console.log(name);
 console.log(age);
+
+const heading = document.querySelector('.header1--js');
+heading.innerHTML=`Siemanko. Miło, że tu zajrzałeś. Nazywam się ${name} i mam ${age} lat.`;
