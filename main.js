9a237b018bdb790a003621e02b3f6dd9d2ec32be
Piotr Omelianiuk <pomelianiuk@gmail.com>
2020-05-30 22:20:47 +0200

    add new function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 alert('siema');
 const name='Piotrek';
 const age= 25 ;
+const myNumber= 5
 
 console.log(`Siemanko. Miło, że tu zajrzałeś. Nazywam się ${name} i mam ${age} lat.`);
 console.log(name);
@@ -8,3 +9,15 @@ console.log(age);
 
 const heading = document.querySelector('.main__heading--js');
 heading.innerHTML=`Siemanko. Miło, że tu zajrzałeś. Nazywam się ${name} i mam ${age} lat.`;
+
+console.log(`moje imię to ${name}`);
+console.log(`mam ${age} lat`);
+
+
+
+constCalculate=(myNumber)=>{
+   myNumber=myNumber*3;
+    console.log(myNumber);
+    return myNumber;
+}
+console.log(myNumber*3)
